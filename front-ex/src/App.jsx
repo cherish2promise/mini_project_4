672b4e6fbcfeb2f55cf1b9f8bce053fcfcc43384
Lee heon jun <59328372+cherish2promise@users.noreply.github.com>
2025-05-30 10:36:20 +0900

    front-ex

---
+++ front-ex/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState,useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from "axios"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    axios.get('/api/hello')
+      .then(res => {
+        console.log('백엔드 응답:', res.data);
+      })
+      .catch(err => {
+        console.error('오류 발생:', err);
+      });
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
